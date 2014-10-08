9f953793af9a95e9ec5825931f177a502a2c04ac
maojj-sjtu <maojunjiemomo@gmail.com>
2014-10-08 15:07:26 +0800

    .

--- autoRefresh.user.js
+++ autoRefresh.user.js
@@ -1,11 +1,5 @@
-// ==UserScript==
-// @name 	renrenautorefresh
-// @namespace	renrenautorefresh
-// @description 用于人人网自动刷新 攒RP
-// @include 	*
-// ==/UserScript==
-
 function myrefresh(){
-window.lcation.reload();
-}
-setTimeout('myrefresh()',5000);
+  alert('refresh!');
+  location.reload();
+};
+setInterval('myrefresh()',5000);
