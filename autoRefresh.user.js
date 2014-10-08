1d38ed3c08c0fa19a414bd32e2c2160dfc714d43
maojj-sjtu <maojunjiemomo@gmail.com>
2014-10-08 14:27:46 +0800

    refresh function finish

---
+++ autoRefresh.user.js
@@ -0,0 +1,11 @@
+// ==UserSrcipt==
+// @name 	renrenautorefresh
+// @namespace	renrenautorefresh
+// @description 用于人人网自动刷新 攒RP
+// @include 	*
+// ==/UserScript==
+
+function myrefresh(){
+window.lcation.reload();
+}
+setTimeout('myrefresh()',1860000);
